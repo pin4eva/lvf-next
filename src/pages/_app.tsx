424d002987c3a226b2aa12d0b06df5c35dbc7e31
pin4eva <pin4eva@hotmail.com>
2021-03-02 07:59:31 +0100

    added loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,21 @@
 // import HeaderComp from "components/HeaderComp";
-import { AppProps } from "next/dist/next-server/lib/router/router";
-import React, { Fragment } from "react";
-import "../styles/index.scss";
+import { AppProps } from "next/app";
 import Head from "next/head";
+import React, { Fragment, useState } from "react";
+import "../styles/index.scss";
 
 const MyApp: React.FC<AppProps> = ({
   Component,
   pageProps,
 }: AppProps): JSX.Element => {
+  const [loading, setLoading] = useState(true);
+  if (typeof window !== "undefined") {
+    window?.addEventListener("load", () => {
+      setLoading(false);
+    });
+  }
+
+  if (loading) return <p>Loading...</p>;
   return (
     <Fragment>
       <Head>
